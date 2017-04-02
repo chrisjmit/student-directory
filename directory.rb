4e5f76f1299d7eb2bdb782805591c9fe08e8db63
Chris Mitchell <chrisjmit@gmail.com>
2017-04-02 14:05:42 +0100

    Loads students from cmd line argument, ignore added

--- directory.rb
+++ directory.rb
@@ -1,133 +1,150 @@
+@students = [] # an empty array accessible to all methods
+
+def print_menu
+  puts "1. Input the students details"
+  puts "2. Show the students record"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  #puts "3. Use control flow to print"
+  #puts "4. Show list of student by their first initial"
+  #puts "5. Show students with less than 12 letters in their name"
+  puts "9. Exit"
+end
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students details"
-    puts "2. Show the students record"
-    puts "9. Exit" #9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    user_select = gets.chomp
-    # 3. do what the user has asked
-    case user_select
-    when "1"
-      # input the students
-      students = input_students
-    when "2"
-      # show the students
-      print_header
-      print_(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-    # 4. repeat from step 1
-      puts "I don't know what you meant, try again."
-    end
+    print_menu
+    process(STDIN.gets.chomp)
   end
 end
 
-
+def process(user_select)
+  case user_select
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again."
+  end
+end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-    # create an empty array
-  students = []
-  default = "November"
-    # get the first name
-  name = gets.chomp
+  cohort_default = "November"
+  name = STDIN.gets.chomp
     name.empty? ? exit : name
-    students.count != 1 ? "Now we have #{students.count} students" : "Now we have 1 student"
-
-    # while the name is not empty, repeat this code
   while !name.empty? do
-    # capture further information about each student
     puts "What cohort do you belong to?"
-      cohort = gets.delete("\n")
+      cohort = STDIN.gets.delete("\n")
         if cohort.empty?
-          puts "The default cohort is #{default}."
-          cohort = default
+          puts "The default cohort is #{cohort_default}."
+          cohort = cohort_default
         end
 
     puts "Please list their hobbies."
-      hobbies = gets.delete("\n")
+      hobbies = STDIN.gets.delete("\n")
     puts "What is their country of birth?"
-      country_of_birth = gets.delete("\n")
+      country_of_birth = STDIN.gets.delete("\n")
     puts "What is their height?"
-      height = gets.delete("\n")
-    # add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym, hobbies: hobbies, country_of_birth: country_of_birth.to_sym, height: height.to_sym}
+      height = STDIN.gets.delete("\n")
 
-    puts students.count != 1 ? "Now we have #{students.count} students" : "Now we have 1 student"
+    @students << {name: name, cohort: cohort.to_sym, hobbies: hobbies, country_of_birth: country_of_birth.to_sym, height: height.to_sym}
 
-    # get another name from the user
+    puts @students.count != 1 ? "Now we have #{@students.count} students" : "Now we have 1 student"
+
     puts "Please add the name of the next student. Leave this entry blank to finish adding students."
-    name = gets.chomp
-  end
-  #return the array of students
-  students
-end
-
-
-def print_using_control_flow(students)
-  count = 1
-  while count <= students.length
-  puts "#{count}: #{students[count-1][:name]} of the #{students[count-1][:cohort]} cohort"
-    count += 1
+    name = STDIN.gets.chomp
   end
 end
 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
 
 def print_header
 puts "The students of Villains Academy".center(100)
 puts "-------------".center(100)
 end
 
-
-def print_(students)
-    students.each_with_index do |student, index|
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort). He is #{student[:height]} tall, was born in #{student[:country_of_birth]}, and has the following hobbies: #{student[:hobbies]})".center(100)
-    end
+def print_students_list
+  @students.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort). He is #{student[:height]} tall, was born in #{student[:country_of_birth]}, and has the following hobbies: #{student[:hobbies]})".center(100)
+  end
 end
 
-#def print_by_group(students)
-#  students.map do |x|
-#      x[:cohort]
-#    end
-#  puts students
-#end
+def print_using_control_flow
+  count = 1
+  while count <= @students.length
+  puts "#{count}: #{@students[count-1][:name]} of the #{@students[count-1][:cohort]} cohort"
+    count += 1
+  end
+end
 
-
-def filter_by_input(students)
+def filter_by_input
   puts "You can list the students by their first initial. What is the first letter you would like to filter by?"
-    firstletter = gets.chomp
-  students.each_with_index do |student, index|
+    firstletter = STDIN.gets.chomp
+  @students.each_with_index do |student, index|
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?(firstletter)
   end
 end
 
-
-def filter_by_length_12(students)
-  students.each_with_index do |student, index|
+def filter_by_length_12
+  @students.each_with_index do |student, index|
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
   end
 end
 
 
-def print_footer(students)
-  if students.count != 1
-    puts "Overall, we have #{students.count} great students".center(100)
+def print_footer
+  if @students.count != 1
+    puts "Overall, we have #{@students.count} great students".center(100)
   else
-    puts "Overall, we have #{students.count} great student".center(100)
+    puts "Overall, we have #{@students.count} great student".center(100)
   end
 end
-#nothing happens until we call the methods
 
+def save_students
+ # open the file for writing
+ file = File.open("students.csv", "w")
+ # iterate over the array of students
+ @students.each do |student|
+   student_data = [student[:name], student[:cohort]]
+   csv_line = student_data.join(",")
+   file.puts csv_line
+ end
+ file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exists?(filename) #if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exists
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
+try_load_students
 interactive_menu
-#print_header
-#print_(students)
-#filter_by_input(students)
-#filter_by_length_12(students)
-#print_using_control_flow(students)
-#print_by_group(students)
-#print_footer(students)
